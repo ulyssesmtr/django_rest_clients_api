af6bdc475fd8c3fa7cb278e1e1d6f10ee0aad32f
ulyssesmtr <ulyssesdmnt@gmail.com>
2021-11-07 21:42:04 -0300

    refactoring

--- setup/settings.py
+++ setup/settings.py
@@ -11,16 +11,16 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+SECRET_KEY = config("SECRET_KEY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ue8hje1_a@&&cwm*3*)f@b^yzci-ml$r7w=@w)+okp=f3-urb-'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
